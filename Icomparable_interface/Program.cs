8174401929f0043c6465fb4579f979a481156be1
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2024-01-17 20:04:28 +0700

    .

---
+++ Icomparable_interface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        cat1.Damage=2;
+        cat2.Damage=5;
+        int ketqua = cat1.CompareTo(cat2);
+        if(ketqua>0)
+        {
+            Console.WriteLine("meo 1 khoe hon");
+        }else if(ketqua==0)
+        {
+            Console.WriteLine("khoe bang nhau");
+        }else
+        {
+            Console.WriteLine("meo 2 khoe hon");
+        }
+    }
+}
+public class Cat : IComparable<Cat>
+{
+    public int Damage =3;
+    public int CompareTo(Cat other)
+    {
+        return this.Damage.CompareTo(other.Damage);
+    }
+}
--- Interface Edible/Program.cs
+++ Interface Edible/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Animal[] animal1 = new Animal[2];
+        animal1[0] = new Tiger();
+        animal1[1] = new Chickens();
+        foreach(var item in animal1)
+        {
+            if(item is Chickens)
+            {
+
+            }
+        }
+        Fuit[] fuit1 = new Fuit[2];
+        fuit1[0] = new Apple();
+        fuit1[1] = new Orange();
+        foreach (var item in fuit1)
+        {
+            if(item is Apple)
+            {
+                
+            }
+        } 
+    }
+}
+public abstract class Animal
+{
+    public abstract string MakeSound();
+}
+public interface IEdiable
+{
+    public string HowToEat();
+}
+public abstract class Fuit : IEdiable
+{
+    public abstract string HowToEat();
+ 
+}
+public class Chickens : Animal, IEdiable
+{
+    public string HowToEat()
+    {
+        return "Cound something";
+    }
+
+    public override string MakeSound()
+    {
+        return "chickens : Cluck Cluck...";
+    }
+}
+public class Tiger : Animal
+{
+    public override string MakeSound()
+    {
+        return " Tiger: Grower...";
+    }
+}
+public class Apple : Fuit
+{
+    public override string HowToEat()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Orange : Fuit
+{
+    public override string HowToEat()
+    {
+        throw new NotImplementedException();
+    }
+}
+// vitual --> có thể  có overide hay không vẫn chạy
+// abstract --> phải có override
+// 1 lớp chỉ có thể kế thừa từ 1 lớp
+// 1 lopwskl có thể kế thwuaf từ nhiều interface
+// nếu 1 lớp trước có thể kế thwauf từ interface , thi lớp dẫn xuất không thực hiện intelemant của interface đó
